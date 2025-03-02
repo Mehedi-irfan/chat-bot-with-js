cea7ca3652e869574e71dc1c6ba3991aec46d55e
Mehedi-irfan <mehediirfan99@gmail.com>
2025-03-02 17:00:09 +0600

    update some functionality

--- src/script.js
+++ src/script.js
@@ -1,5 +1,10 @@
 const messageInput = document.getElementById("message-input");
 const chatBody = document.getElementById("chat-body");
+const sendMessageBtn = document.getElementById("send-message");
+
+const userData = {
+  message: null,
+};
 
 const createMessageElement = (content, classes) => {
   const div = document.createElement("div");
@@ -8,11 +13,13 @@ const createMessageElement = (content, classes) => {
   return div;
 };
 
-const handleOutGoingMessage = (message) => {
+const handleOutGoingMessage = (e) => {
+  e.preventDefault();
+  userData.message = messageInput.value.trim();
   const userMassage = `<p
-            class="text-sm bg-purple-600 text-white px-4 py-2 rounded-t-xl rounded-bl-xl m-0 "
+            class="text-sm bg-purple-600 text-white px-4 py-2 rounded-t-xl rounded-bl-xl flex-wrap break-words inline-block max-w-[80%] m-0 "
           >
-           ${message}
+           ${userData.message}
           </p>`;
   const outGoingMessageDiv = createMessageElement(userMassage, "user-message");
   chatBody.appendChild(outGoingMessageDiv);
@@ -22,6 +29,8 @@ messageInput.addEventListener("keydown", (e) => {
   const message = e.target.value.trim();
   if (e.key === "Enter" && message) {
     e.preventDefault();
-    handleOutGoingMessage(message);
+    handleOutGoingMessage(e);
   }
 });
+
+sendMessageBtn.addEventListener("click", (e) => handleOutGoingMessage(e));
