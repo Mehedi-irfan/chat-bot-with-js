6bfa5702f6999a34cbb6303598513597d7b681ec
Mehedi-irfan <mehediirfan99@gmail.com>
2025-03-02 21:51:04 +0600

    do more functionality

--- src/script.js
+++ src/script.js
@@ -8,7 +8,7 @@ const userData = {
 
 const createMessageElement = (content, classes) => {
   const div = document.createElement("div");
-  div.classList.add("m-[6px]", "flex", "justify-end", classes);
+  div.classList.add("my-[6px]", "justify-end", "flex", classes);
   div.innerHTML = content;
   return div;
 };
@@ -16,6 +16,7 @@ const createMessageElement = (content, classes) => {
 const handleOutGoingMessage = (e) => {
   e.preventDefault();
   userData.message = messageInput.value.trim();
+  messageInput.value = " ";
   const userMassage = `<p
             class="text-sm bg-purple-600 text-white px-4 py-2 rounded-t-xl rounded-bl-xl flex-wrap break-words inline-block max-w-[80%] m-0 "
           >
@@ -23,6 +24,27 @@ const handleOutGoingMessage = (e) => {
           </p>`;
   const outGoingMessageDiv = createMessageElement(userMassage, "user-message");
   chatBody.appendChild(outGoingMessageDiv);
+
+  setTimeout(() => {
+    const userMassage = `<div class="flex items-start justify-start">
+            <i
+              class="ri-chat-smile-2-line text-lg bg-purple-600 text-white rounded-full px-2 py-1"
+            ></i>
+          </div>
+          <div>
+            <div
+              class="bg-gray-100 px-4 py-3 rounded-t-xl rounded-br-xl flex gap-1 animate-[fade_1.8s_ease-in-out_infinite]"
+            >
+              <p class="dot w-[6px] h-[6px] rounded-full bg-gray-400"></p>
+              <p class="dot w-[6px] h-[6px] rounded-full bg-gray-400"></p>
+              <p class="dot w-[6px] h-[6px] rounded-full bg-gray-400"></p>
+            </div>
+          </div>`;
+    const incomingMessage = createMessageElement(userMassage, "bot-message");
+    chatBody.appendChild(incomingMessage);
+    incomingMessage.classList.remove("justify-end");
+    generateBotReply();
+  }, 600);
 };
 
 messageInput.addEventListener("keydown", (e) => {
